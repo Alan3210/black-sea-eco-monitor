d3c6708704a5d4b101a64ba13a7f832980215a5a
Alan3210 <alan.kodzayev@gmail.com>
2026-09-11 21:30:50 +0300

    Add evidence and source database models

--- backend/database/models.py
+++ backend/database/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, String, Float
+from sqlalchemy import Column, String, Float, Integer, ForeignKey
+from sqlalchemy.orm import relationship
+
 
 from backend.database.database import Base
 
@@ -46,4 +48,79 @@ class EventDB(Base):
     description = Column(
         String,
         nullable=True
+    )
+
+    evidences = relationship(
+    "EvidenceDB",
+    backref="event"
+    )
+
+class SourceDB(Base):
+
+    __tablename__ = "sources"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+
+    type = Column(
+        String,
+        nullable=False
+    )
+
+    name = Column(
+        String,
+        nullable=False
+    )
+
+    url = Column(
+        String,
+        nullable=True
+    )
+
+    reliability = Column(
+        Float,
+        default=0.5
+    )
+
+    evidences = relationship(
+    "EvidenceDB",
+    backref="source"
+    )
+
+class EvidenceDB(Base):
+
+    __tablename__ = "evidences"
+
+    id = Column(
+        Integer,
+        primary_key=True,
+        index=True
+    )
+
+    event_id = Column(
+        String,
+        ForeignKey("events.id")
+    )
+
+    source_id = Column(
+        Integer,
+        ForeignKey("sources.id")
+    )
+
+    type = Column(
+        String,
+        nullable=False
+    )
+
+    description = Column(
+        String,
+        nullable=False
+    )
+
+    confidence = Column(
+        Float,
+        nullable=False
     )
